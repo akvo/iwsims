75d69195de028667bb8cd9610c3c5af254fdf78d
Iwan Firmawan <firmawaneiwan@gmail.com>
2026-04-28 11:47:39 +0700

    fix(run-qc.sh): enable coveralls reporting if token is set

--- backend/run-qc.sh
+++ backend/run-qc.sh
@@ -16,9 +16,9 @@ echo "Coverage"
 coverage combine --rcfile=./.coveragerc
 coverage report -m --rcfile=./.coveragerc
 
-# if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]] ; then
-#   coveralls
-# fi
+if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]] ; then
+  coveralls
+fi
 
 echo "Generate Django DBML"
 python manage.py dbml > db.dbml
